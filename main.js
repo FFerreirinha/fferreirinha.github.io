ea8a3f997883dd2474c1901751e9b3ddb3a9beba
Fabio Ferreirinha <fabio.fereirinha@gmail.com>
2022-09-20 06:50:41 +0100

    Create main game loop
    Utilized modules gameController and gameBoard to begin implementing main game loop. #TODO: Check fixGameState and discover why win conditions are not working.

--- main.js
+++ main.js
@@ -1,7 +1,95 @@
-const Gameboard = (() => {
-    const moves = {};
-    const crossPlay = (x, y) => moves[x][y] = 1;
-    const circlePlay = (x, y) => moves[x][y] = 2;
-    return {circlePlay, crossPlay};  
+game_display = document.getElementById("gameboard");
+
+
+
+const gameBoard = (() => {
+    let moveBoard = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+
+    const getBoard = () => {moveBoard};
+
+    const setUp = () => {
+        game_cells = Array.from(document.getElementsByClassName("gameboard-cell"));    
+        game_cells.forEach(cell => cell.addEventListener("click", () => {
+            if(cell.innerText !== "X" && cell.innerText !== "O") {
+
+                if(gameController.moveCounter % 2 === 0) {
+                    cell.innerText = "X";
+                    gameBoard.moveBoard[cell.id] = "X";
+                }
+                else if(gameController.moveCounter % 2 !== 0) {
+                    cell.innerText = "O";
+                    gameBoard.moveBoard[cell.id] = "O";
+                }
+
+
+                gameController.increaseCounter();
+                gameController.checkForGameState(gameBoard.moveBoard);
+            }
+            else {
+                console.log("fail");
+            }
+        }))
+    };
+
+    const reset = () => {
+        gameBoard.moveBoard = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+
+        //Clearing each cell
+        game_cells.forEach(cell => cell.remove());
+        for(let i = 0; i < 9; i++) {
+            tempCell = document.createElement("BUTTON");
+            tempCell.classList.add("gameboard-cell");
+            tempCell.id = i;
+            game_display.appendChild(tempCell);
+        }
+        gameBoard.setUp();
+    }
+
+    return{setUp, reset, moveBoard, getBoard};
 })();
 
+const player = ((symbol) => {
+    const makeMove = (coord) => {
+        gameBoard.moveBoard[coord] = symbol;
+    } 
+
+    return{makeMove};
+});
+
+const gameController = (() => {
+    let moveCounter = 0;
+
+    const increaseCounter = () => { gameController.moveCounter += 1;}
+
+    const resetCounter = () => { gameController.moveCounter = 0;}
+
+    const checkForGameState = (board) => {
+        const rowWinCondition = (((board[0] === board[1] === board[2]) && board[0] !== "0") || ((board[3] === board[4] === board[5]) && board[3] !== "0") || ((board[6] === board[7] === board[8]) && board [6] !== "0"));
+        const columnWinCondition = (((board[0] === board[3] === board[6]) && board[0] !== "0") || ((board[1] === board[4] === board[7]) && board[1] !== "0") || ((board[2] === board[5] === board[8]) && board[2] !== "0"))
+        const diagonalWinCondition = (((board[0] === board[4] === board[8]) || board[2] === board[4] === board[6]) && board[4] !== "0");
+        
+        if (rowWinCondition || columnWinCondition || diagonalWinCondition) {
+            // Code for win
+            overlay.classList.add("overlay-active");
+        }
+        else if (!(gameBoard.moveBoard.includes("0"))) {
+            // Checking for a tie
+            overlay.classList.add("overlay-active");
+        }
+
+ 
+    }
+
+    // End of Game overlay
+    overlay = document.getElementById("overlay");
+    overlay.addEventListener("click", () => {
+            resetCounter();
+            gameBoard.reset(); 
+
+            overlay.classList.remove("overlay-active");
+    })
+    
+    return {moveCounter, increaseCounter, checkForGameState};
+})();
+
+gameBoard.setUp();
